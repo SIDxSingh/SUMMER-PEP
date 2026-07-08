01713a73cc12272ba3295c7c48ce2856689c5969
SIDxSingh <siddharthwizard123@gmail.com>
2026-07-08 09:47:47 +0530

    Create BST_leetcode700.java

---
+++ BST_leetcode700.java
@@ -0,0 +1,18 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        }
+        return searchBST(root.right, val);
+    }
+}
